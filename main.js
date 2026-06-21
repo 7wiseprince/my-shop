5d94ae8d9d3dc8fd7f23c7489f43c593bdac72e4
7wiseprince <7wiseprince@gmail.com>
2026-06-21 17:20:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+let allProducts = [];
+let cart = [];
+let favorites = []; // Масив для зберігання улюблених товарів
 
+document.addEventListener("DOMContentLoaded", () => {
+    loadProducts();
+    loadAdminOrders(); 
+    if (typeof updateCartUI === 'function') {
+        updateCartUI(); 
+    } else {
+        updateCartBadge();
+    }
+});
