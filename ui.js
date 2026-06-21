c4d3dbc2583b9ef30950f95e5cce5543283acbbd
7wiseprince <7wiseprince@gmail.com>
2026-06-21 17:53:14 +0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,1 +1,19 @@
+function switchPage(pageId) {
+    if (pageId === 'favorites'){
+        updateFavoritesUI();
+    }
+    if (pageId === 'home'){
+        updateFeaturedProductsUI();
+        }
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(page => {
+        page.classList.remove('active');
+    });
 
+    const activePage = document.getElementById('page-' + pageId);
+    if (activePage) {
+        activePage.classList.add('active');
+    } else {
+        console.error("Не знайдено сторінку: page-" + pageId);
+    }
+}
